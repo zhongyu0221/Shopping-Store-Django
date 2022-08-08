bd48f25e070d3a267c527249318e115b4c7c4f4c
zhongyu0221 <zhongyu0221@gmail.com>
2022-08-08 00:01:21 -0500

    fixed JS name, pending csrf

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,25 +1,26 @@
-console.log('hello world!!??')
+var user = document.getElementById("myVar").value;
+console.log('check user:',user)
 
 var updatedBtns = document.getElementsByClassName('update-cart')
 // get all the Update Cart btn
 
 for (var i = 0;i<updatedBtns.length; i++){//go through each btn
+
+
     updatedBtns[i].addEventListener('click',function (){
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productID:', productId,'Action:',action)// check in console
 
+
         console.log('USER:',user)
         if(user === 'AnonymousUser'){
            console.log('user is not loged in')
         }else{
-            //console.log('user is authenticated, sending data...',user)
+            console.log('user is authenticated, sending data...',user)
             updateUserOrder(productId,action)
         }
-
     })
-
-
 }
 
 
@@ -35,7 +36,7 @@ function updateUserOrder(productId,action){
         method:'POST',
         headers: {
             'Content-Type': 'application/json',
-            'X-CSRFToken':csrftoken,//add csrf token in main html
+            'X-CSRFToken': csrftoken//add csrf token in main html
         },
         //body is the part of data we wanna send. send as a string
         body:JSON.stringify({'productID': productId,'Action:':action})
